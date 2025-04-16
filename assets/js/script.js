03601f5ae15d029b7d0ca9993c89fe46052d6a75
Rychner <milanrychner@gmail.com>
2025-04-16 13:26:38 +0700

    initial commit 15

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,20 @@ if (music) {
 	audio.src = music
 }
 
+
+function openFullscreen() 
+{
+	const elem = document.documentElement;
+
+	if (elem.requestFullscreen) {
+		elem.requestFullscreen();
+	} else if (elem.webkitRequestFullscreen) { // Safari
+		elem.webkitRequestFullscreen();
+	} else if (elem.msRequestFullscreen) { // IE11
+		elem.msRequestFullscreen();
+	}
+}
+
 // door mulai
 function mulai()
 {
